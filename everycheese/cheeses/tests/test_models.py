7fc74f2b87efb8850342cf061a5e7b43e35b4c92
mispotackow <potackiw@gmail.com>
2020-09-15 22:11:52 +0300

    Use CheeseFactory in a test

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,15 +1,12 @@
 import pytest
 from ..models import Cheese
+from .factories import CheeseFactory
 
 # Связывает наши тесты с нашей базой данных
 pytestmark = pytest.mark.django_db
 
 
 def test___str__():
-    cheese = Cheese.objects.create(
-        name='Stracchino',
-        description='Semi-sweet cheese that goes well with starches.',
-        firmness=Cheese.Firmness.SOFT,
-    )
-    assert cheese.__str__() == 'Stracchino'
-    assert str(cheese) == 'Stracchino'
+    cheese = CheeseFactory()
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
